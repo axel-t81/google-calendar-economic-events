eaa78b4447939682f06f86d2b21faaf71325c3c5
Axel <63882492+axel-t81@users.noreply.github.com>
2024-02-16 19:51:22 +1100

    Update main.py
    Slight progress, but see Comments within.

--- main.py
+++ main.py
@@ -16,28 +16,31 @@
 # (a) Look at the current data and see, as a human, how you could use this in your idea in a practical sense.
 # (b) Then you need to turn this text data into something that can be sliced/used for your answer to (a)
 
+## COMMENTS AFTER DAY 2
+# The current code is able to get the data for 3 months and print the response as a List, that was originally a CSV download
+# The problem today is that there is too much data, and you want to cull things down to the NASDAQ 100 or S&P500 or DowJones, etc
+# But you don't know how to access appropriate lines other than using the index value
+# But you need something akin to matching the Ticker instead ...and this is the problem you're working on
+# You've looked at Filters, Contains(?), Splice, and a few others, inc converting List to Dictionary, but you can't work it out yet
+
 import requests
 import json
 import csv
 import time
+import re
+import pandas as pd
 # This next import statement hides your API Key in another file that should be Ignored by Git
 from env import API_KEY
 
 # The API endpoint, You can see Earnings Calendar & Python examples and endpoints at https://www.alphavantage.co/documentation/
-CSV_URL = "https://www.alphavantage.co/query?function=EARNINGS_CALENDAR&horizon=3month&apikey=API_KEY"
+CSV_URL = "https://www.alphavantage.co/query?function=EARNINGS_CALENDAR&symbol=IBM&&symbol=AAL&horizon=3month&apikey=API_KEY"
 
-# A GET request to the API. Test if response is successful. If so, Print the response
-response = requests.get(CSV_URL)
-# Print the status code of the call response
-print(response.status_code)
-# Print the data you've just called via the GET Request
-print(response.text)
-
-### The stuff below is commented out because you can't yet get it to work
-#if response.status_code != 200:
-#    print("Server didn't return an 'OK' response.  Content was: {!r}".format(response.content))
-#else:
-#   json_data = response.json()
-#   time.sleep(28)
-#
-#print(json_data)
+with requests.Session() as s:
+    download = s.get(CSV_URL)
+    decoded_content = download.content.decode('utf-8')
+    cr = csv.reader(decoded_content.splitlines(), delimiter=',')
+    my_list = list(cr)
+    print(type(download))
+    print(type(decoded_content))
+    print(type(cr))
+    print(type(df))
