9595c79484c0dba8f4c58cff40a9456783b5eb6c
Axel <63882492+axel-t81@users.noreply.github.com>
2024-10-31 23:39:15 +1100

    Basic review after break
    Reviewed and attempted factor at project that is a few months untouched.

--- main.py
+++ main.py
@@ -1,46 +1,38 @@
 # Python file
-
-# 1) Firstly, I need to collect API data
-
-# 2) Then I need to push that to a public Google Calendar
-
-# 3) THen I need to somehow repeat this process every 24 hours, forever
-
-
-# Okay Step 1 below
-
-## COMMENTS AFTER DAY 1
-# The current code is able to get the data for 3 months and print the response as Text
-# You can see Earnings Calendar & Python examples and endpoints at https://www.alphavantage.co/documentation/
-# What your missing now is:
-# (a) Look at the current data and see, as a human, how you could use this in your idea in a practical sense.
-# (b) Then you need to turn this text data into something that can be sliced/used for your answer to (a)
-
-## COMMENTS AFTER DAY 2
-# The current code is able to get the data for 3 months and print the response as a List, that was originally a CSV download
-# The problem today is that there is too much data, and you want to cull things down to the NASDAQ 100 or S&P500 or DowJones, etc
-# But you don't know how to access appropriate lines other than using the index value
-# But you need something akin to matching the Ticker instead ...and this is the problem you're working on
-# You've looked at Filters, Contains(?), Splice, and a few others, inc converting List to Dictionary, but you can't work it out yet
+# * Need to put up guarded function to run main
+# * Need to collect API data
+# * But this CSV data is 9000+ rows long
+# * So need to work out a way to curate this
+# * Then this curated data needs to sent to an ICS file
+# * ANd the user will need to collect all of them, or have a way to select which ones they want
+# * This process needs to be repeated, whenever a user accesses the script; on demand.
 
 import requests
-import json
 import csv
-import time
-import re
-import pandas as pd
+import ssl
+
 # This next import statement hides your API Key in another file that should be Ignored by Git
 from env import API_KEY
 
 # The API endpoint, You can see Earnings Calendar & Python examples and endpoints at https://www.alphavantage.co/documentation/
-CSV_URL = "https://www.alphavantage.co/query?function=EARNINGS_CALENDAR&symbol=IBM&&symbol=AAL&horizon=3month&apikey=API_KEY"
+csv_url = "https://www.alphavantage.co/query?function=EARNINGS_CALENDAR&horizon=3month&apikey=API_KEY"
 
-with requests.Session() as s:
-    download = s.get(CSV_URL)
+# Ignore SSL certificate errors
+# Remove this section, and remove context=ctx if I get errors with https sites
+ctx = ssl.create_default_context()
+ctx.check_hostname = False
+ctx.verify_mode = ssl.CERT_NONE
+
+my_list = []
+
+with requests.Session() as phone:
+    download = phone.get(csv_url)
+    print(type(download))
     decoded_content = download.content.decode('utf-8')
     cr = csv.reader(decoded_content.splitlines(), delimiter=',')
-    my_list = list(cr)
-    print(type(download))
-    print(type(decoded_content))
     print(type(cr))
-    print(type(df))
+    my_list = list(cr) # This works in putting all data in a List
+    print(my_list) # And you can print out this massive list
+    my_dict = dict(cr) # I tried to replicate this ...but with a Dictionary
+    print(type(my_dict)) 
+    print(my_dict) # But nothing is appended to the dictionary
